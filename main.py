df1b9382bbfb3dab9cc68869bc184df618d4abdf
welly-s <iamwelly@hotmail.com>
2023-09-28 22:16:23 +0800

    Add description on the new column

--- main.py
+++ main.py
@@ -9,8 +9,14 @@ with col1:
 
 with col2:
     st.title("Ardic Sulce")
-    content = """ Hello.. I am xxx
-    nice to meet u.Type all the text here.
-    bla bla blaaaaa"""
+    content = """ Hello I am Ardit! I am a python programmer, teacher and founder of PytonnHow. 
+    I graduated in 2013 with a Master of Science in Geospatial Technologies from University of Muenster in German 
+    with a focis on using Python for remote sensing. I have worked with companies from various countries,
+     such as the Center for Conservation Geography, to map and understand Australian Ecosystems, image processing with 
+     Swiss inTerra and performing data mining to gain business insights with the Australian Rapid Intelligence"""
     #st.write(content)
     st.info(content)
+
+#st.text('Below you can find some of the apps that i have built in pythin. feel free to contact me'  )
+st.write('Below you can find some of the apps that i have built in pythin. feel free to contact me'    )
+#st.markdown('Below you can find some of the apps that i have built in pythin. feel free to contact me')
