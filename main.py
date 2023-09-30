e5a3905cb794b3227b36e6271681f9627bf46abd
welly-s <iamwelly@hotmail.com>
2023-09-30 15:41:06 +0800

    Day22-render the app titles into columns

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout='wide')
 
@@ -20,3 +21,15 @@ with col2:
 #st.text('Below you can find some of the apps that i have built in pythin. feel free to contact me'  )
 st.write('Below you can find some of the apps that i have built in pythin. feel free to contact me'    )
 #st.markdown('Below you can find some of the apps that i have built in pythin. feel free to contact me')
+
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv("data.csv", sep = ';')
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row['title'])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row['title'])
