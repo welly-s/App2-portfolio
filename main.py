b2e8ff5593902f01c03de867499ba9c267ebabf0
welly-s <iamwelly@hotmail.com>
2023-09-30 15:58:46 +0800

    Day22-add description, images and links

--- main.py
+++ main.py
@@ -22,14 +22,20 @@ with col2:
 st.write('Below you can find some of the apps that i have built in pythin. feel free to contact me'    )
 #st.markdown('Below you can find some of the apps that i have built in pythin. feel free to contact me')
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5,0.5,1.5])   #ratio dimension of the col
 
 df = pandas.read_csv("data.csv", sep = ';')
 
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row['title'])
+        st.write(row['description'])
+        st.image("images/" + row['image'])
+        st.write(f"[Source Code]( {row['url'] } )")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row['title'])
+        st.write(row['description'])
+        st.image("images/" + row['image'])
+        st.write(f"[Source Code]( {row['url'] } )")
